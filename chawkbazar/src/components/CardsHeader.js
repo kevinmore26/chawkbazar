c93aff954b96745e3ff55118ac5ee5f3bbbfc97d
kevinmore26 <kore_2608@hotmail.com>
2022-09-20 18:05:58 -0500

    commit

--- chawkbazar/src/components/CardsHeader.js
+++ chawkbazar/src/components/CardsHeader.js
@@ -2,60 +2,26 @@ import Container from "react-bootstrap/esm/Container";
 import "./cards.css";
 export default function CardsHeader() {
   return (
-    <div style={{ padding: "0px 10px" }}>
-      <div className="row">
-        <div className="col-6">
-          <img
-            style={{
-              height: "320px",
-              objectFit: "cover",
-              padding: "0px !important",
-            }}
-            src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-1.jpg&w=1080&q=100"
-          ></img>
-        </div>
-        <div className="col-3">
-          <img
-            style={{
-              height: "320px",
-              margin: "0 auto",
-              display: "flex",
-              position: "relative",
-              left: "10px",
-              objectFit: "cover",
-              padding: "0px",
-            }}
-            src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-2.jpg&w=640&q=100"
-          ></img>
-        </div>
-        <div className="col-3">
-          <img
-            style={{ height: "320px", objectFit: "cover", padding: "0px" }}
-            src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-3.jpg&w=640&q=100"
-          ></img>
-        </div>
+    <div class="containergrid">
+      <div class="primero">
+        <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-1.jpg&w=1080&q=100" />
       </div>
-      <div
-        className="container1"
-        style={{
-          display: "grid",
-          marginTop: "10px",
-          gridAutoColumns: "1fr",
-          gridTemplateColumns: " 1fr 1fr 1fr 1fr 1fr",
-          gridTemplateRows: "1fr",
-          gap: "1em 1em ",
-        }}
-      >
-        <div className="tercero">
-          <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-3.jpg&w=640&q=100"></img>
-        </div>
-        <div className="segundo">
-          <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-2.jpg&w=640&q=100"></img>
-        </div>
-        <div className="primero">
-          <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-1.jpg&w=1080&q=100"></img>
-        </div>
+      <div class="segundo">
+        <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-2.jpg&w=640&q=100"></img>
+      </div>
+      <div class="tercero">
+        <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-3.jpg&w=640&q=100"></img>
+      </div>
+      <div class="cuarto">
+        <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-4.jpg&w=1080&q=100" />
+      </div>
+      <div class="quinto">
+        <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-5.jpg&w=1080&q=100" />
+      </div>
+      <div class="sexto">
+        <img src="https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fmasonry%2Fbanner-6.jpg&w=3840&q=100" />
       </div>
     </div>
+ 
   );
 }
